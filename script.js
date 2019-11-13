ef07c9917ea75955c471883ae61662c271f5403d
Alice <for.work.webdeveloper@gmail.com>
2019-11-13 19:45:11 +0800

    made up the menu

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var app = new Vue ({
 		el:'#app',
 		data: {
-			 src:"icon/logo.png",
+			 srcIcon:"icon/logo.png",
   				
 			items: [
-				{title: 'Home', href: 'index.html'},
-				{title: 'About Us', href: 'about.html'},
-				{title: 'Servisec', href: '/about.html', class:'dropdown'},
-				{title: 'Careers', href: '/about.html'},
-				{title: 'News', href: '/about.html'},
-				{title: 'Documentation', href: '/about.html'}
+
+				{title: 'О нас', href: 'about.html', class:'anyDropdown'},
+				{title: 'Услуги', href: '/about.html', class:'dropdown'},
+				{title: 'Вакансии', href: '/about.html', class:'anyDropdown'},
+				{title: 'Новости', href: '/about.html', class:'anyDropdown'},
+				{title: 'Соглашение', href: '/about.html', class:'dropdown'}
 			]
 
 		}
